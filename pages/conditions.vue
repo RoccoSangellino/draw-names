ad452678454a4c010515bd121aa58ef8707e4e7c
Rocco Sangellino <rocco.sangellino@gmail.com>
2022-09-06 23:20:16 -0600

    basic functionality to show excludes list

--- pages/conditions.vue
+++ pages/conditions.vue
@@ -1,107 +1,91 @@
-<template>
-  <div class="fixed top-16 w-72">
-    <Combobox v-model="selected">
-      <div class="relative mt-1">
-        <div
-          class="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm"
-        >
-          <ComboboxInput
-            class="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
-            :displayValue="(person) => person.name"
-            @change="query = $event.target.value"
-          />
-          <ComboboxButton
-            class="absolute inset-y-0 right-0 flex items-center pr-2"
-          >
-            <solid-chevron-down-icon
-              class="h-5 w-5 text-gray-400"
-              aria-hidden="true"
-            />
-          </ComboboxButton>
-        </div>
-        <TransitionRoot
-          leave="transition ease-in duration-100"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-          @after-leave="query = ''"
-        >
-          <ComboboxOptions
-            class="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
-          >
-            <div
-              v-if="filteredPeople.length === 0 && query !== ''"
-              class="relative cursor-default select-none py-2 px-4 text-gray-700"
-            >
-              Nothing found.
-            </div>
+<script setup lang="ts">
+import { useStore } from '@/store/main';
+import {
+  Listbox,
+  ListboxButton,
+  ListboxOptions,
+  ListboxOption,
+} from '@headlessui/vue';
+import { Participant } from '~~/types';
+const store = useStore();
 
-            <ComboboxOption
-              v-for="person in filteredPeople"
-              as="template"
-              :key="person.id"
-              :value="person"
-              v-slot="{ selected, active }"
-            >
-              <li
-                class="relative cursor-default select-none py-2 pl-10 pr-4"
-                :class="{
-                  'bg-teal-600 text-white': active,
-                  'text-gray-900': !active,
-                }"
+const filteredOptions = (id: string) => {
+  return store.participants.filter((person) => person.id !== id);
+};
+</script>
+
+<template>
+  <div>
+    <div class="fixed top-16 w-72">
+      <ul>
+        <li
+          class="mb-16"
+          v-for="participant in store.participants"
+          :key="participant.id"
+        >
+          <div>{{ participant.name }}</div>
+          <Listbox v-model="participant.excludes" multiple by="id">
+            <div class="relative mt-1">
+              <ListboxButton
+                class="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm"
               >
+                <span class="block truncate">{{
+                  participant.excludes.map((person) => person.name).join(', ')
+                }}</span>
                 <span
-                  class="block truncate"
-                  :class="{ 'font-medium': selected, 'font-normal': !selected }"
+                  class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2"
                 >
-                  {{ person.name }}
+                  <solid-chevron-down-icon
+                    class="h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
                 </span>
-                <span
-                  v-if="selected"
-                  class="absolute inset-y-0 left-0 flex items-center pl-3"
-                  :class="{ 'text-white': active, 'text-teal-600': !active }"
+              </ListboxButton>
+
+              <transition
+                leave-active-class="transition duration-100 ease-in"
+                leave-from-class="opacity-100"
+                leave-to-class="opacity-0"
+              >
+                <ListboxOptions
+                  class="z-10 absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
                 >
-                  <solid-check-icon class="h-5 w-5" aria-hidden="true" />
-                </span>
-              </li>
-            </ComboboxOption>
-          </ComboboxOptions>
-        </TransitionRoot>
-      </div>
-    </Combobox>
+                  <ListboxOption
+                    v-slot="{ active, selected }"
+                    v-for="person in filteredOptions(participant.id)"
+                    :key="person.id"
+                    :value="person"
+                    as="template"
+                  >
+                    <li
+                      :class="[
+                        active
+                          ? 'bg-amber-100 text-amber-900'
+                          : 'text-gray-900',
+                        'relative cursor-default select-none py-2 pl-10 pr-4 z-10',
+                      ]"
+                    >
+                      <span
+                        :class="[
+                          selected ? 'font-medium' : 'font-normal',
+                          'block truncate',
+                        ]"
+                        >{{ person.name }}</span
+                      >
+                      <span
+                        v-if="selected"
+                        class="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600"
+                      >
+                        <solid-check-icon class="h-5 w-5" aria-hidden="true" />
+                      </span>
+                    </li>
+                  </ListboxOption>
+                </ListboxOptions>
+              </transition>
+            </div>
+          </Listbox>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
-
-<script setup>
-import { ref, computed } from 'vue';
-import {
-  Combobox,
-  ComboboxInput,
-  ComboboxButton,
-  ComboboxOptions,
-  ComboboxOption,
-  TransitionRoot,
-} from '@headlessui/vue';
-
-const people = [
-  { id: 1, name: 'Wade Cooper' },
-  { id: 2, name: 'Arlene Mccoy' },
-  { id: 3, name: 'Devon Webb' },
-  { id: 4, name: 'Tom Cook' },
-  { id: 5, name: 'Tanya Fox' },
-  { id: 6, name: 'Hellen Schmidt' },
-];
-
-let selected = ref(people[0]);
-let query = ref('');
-
-let filteredPeople = computed(() =>
-  query.value === ''
-    ? people
-    : people.filter((person) =>
-        person.name
-          .toLowerCase()
-          .replace(/\s+/g, '')
-          .includes(query.value.toLowerCase().replace(/\s+/g, ''))
-      )
-);
-</script>
