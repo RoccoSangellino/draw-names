d3f37f7197add6cd4be2fc193b9a58d61e939f60
Rocco Sangellino <rocco.sangellino@gmail.com>
2022-09-06 20:26:57 -0600

    added persisted state pinia plugin

---
+++ pages/conditions.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useStore } from '@/store/main';
+const store = useStore();
+</script>
+
+<template>
+  <div>
+    <header>
+      <h1>Enter Conditions</h1>
+    </header>
+    <div>
+      <ul>
+        <li v-for="participant in store.participants" :key="participant.id">
+          {{ participant.name }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
