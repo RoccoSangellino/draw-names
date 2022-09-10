a4ed64b32c0efadee7d15ef8ed18e40fdf949003
Rocco Sangellino <rocco.sangellino@gmail.com>
2022-09-09 22:07:42 -0600

    basic hero banner setup

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,46 @@
 <template>
   <div>
-    <h1 class="text-4xl text-blue-500">Landing page</h1>
+    <div
+      class="flex flex-col max-w-6xl px-4 mx-auto lg:flex-row sm:px-6 lg:px-8"
+    >
+      <img src="../assets/images/banner.png" alt="" class="lg:order-2 w-full" />
+      <div
+        class="flex flex-col items-center justify-center text-center lg:text-left lg:items-start"
+      >
+        <h1 class="my-6 font-semibold text-slate-900">
+          Gift giving made easy with
+          <span class="text-primary-600 font-extrabold">Secret Selection</span>
+        </h1>
+        <p class="w-4/6 text-lg text-gray-600">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+          suscipit voluptas eius? Sunt quis dolor libero, error quo maxime
+          exercitationem.
+        </p>
+        <div
+          class="flex flex-col w-full px-12 lg:space-x-3 lg:flex-row lg:px-0"
+        >
+          <a
+            href="https://flurly.com/p/tailmars"
+            class="flex items-center justify-center px-6 py-3 mt-6 text-lg font-medium text-white rounded-md bg-primary-600 hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-600"
+          >
+            <span>Get Started</span>
+            <svg
+              class="w-5 h-5 ml-2"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M17 8l4 4m0 0l-4 4m4-4H3"
+              ></path>
+            </svg>
+          </a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
