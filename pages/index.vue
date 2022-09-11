3052e6354974d94f59e6926d71da78c08436a0c3
Rocco Sangellino <rocco.sangellino@gmail.com>
2022-09-10 23:33:43 -0600

    added party banner

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,44 @@
 <template>
-  <div class="bg-yellow p-2 mt-2 rounded-lg">
-    <div>
-      <img src="../assets/images/banner.png" alt="" class="" />
+  <div>
+    <div class="absolute -z-10 w-[394px] left-[-80px] top-[-130px]">
+      <img src="../assets/images/party-banner.png" alt="" />
     </div>
-    <div class="">
-      <h1 class="text-2xl font-semibold text-slate-800">
-        Gift giving made easy with
-        <div class="text-primary-600 font-bold">Secret Selection</div>
-      </h1>
-      <p class="mt-4 mb-4 text-slate-800 opacity-75">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi suscipit
-        voluptas eius? Sunt quis dolor libero, error quo maxime exercitationem.
-      </p>
+    <div class="bg-yellow p-2 mt-16 rounded-lg">
+      <div>
+        <img src="../assets/images/banner.png" alt="" class="" />
+      </div>
       <div class="">
-        <a
-          href="/participants"
-          class="flex items-center justify-center px-6 py-3 mt-6 text-lg font-medium text-white rounded-md bg-primary-600 hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-600"
-        >
-          <span>Get Started</span>
-          <svg
-            class="w-5 h-5 ml-2"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
+        <h1 class="text-2xl font-semibold text-slate-800">
+          Gift giving made easy with
+          <div class="text-primary-600 font-bold">Secret Selection</div>
+        </h1>
+        <p class="mt-4 mb-4 text-slate-800 opacity-75">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+          suscipit voluptas eius? Sunt quis dolor libero, error quo maxime
+          exercitationem.
+        </p>
+        <div class="">
+          <a
+            href="/participants"
+            class="flex items-center justify-center px-6 py-3 mt-6 text-lg font-medium text-white rounded-md bg-primary-600 hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-600"
           >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M17 8l4 4m0 0l-4 4m4-4H3"
-            ></path>
-          </svg>
-        </a>
+            <span>Get Started</span>
+            <svg
+              class="w-5 h-5 ml-2"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M17 8l4 4m0 0l-4 4m4-4H3"
+              ></path>
+            </svg>
+          </a>
+        </div>
       </div>
     </div>
   </div>
